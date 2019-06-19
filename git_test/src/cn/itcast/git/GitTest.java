ab031d9fc6f48e1bbeb08b57d262e0e6cba9b7ef
sola <278872278@qq.com>
2019-06-19 20:30:24 +0800

    修改了一个本地工程

--- git_test/src/cn/itcast/git/GitTest.java
+++ git_test/src/cn/itcast/git/GitTest.java
@@ -3,5 +3,14 @@ package cn.itcast.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+    }
+    public void test(){
+        int i  = 10+11;
     }
 }
